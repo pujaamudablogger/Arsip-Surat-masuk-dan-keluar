63aab4c2e231fb2ea10b3a210e22bec843106bb5
pujaamudablogger <46585647+pujaamudablogger@users.noreply.github.com>
2019-01-25 07:59:31 -0800

    Add files via upload

---
+++ about_tampil.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/logobupati.jpg"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI ARSIP SURAT MASUK DAN SURAT KELUAR <br> KANTOR BUPATI ASAHAN KISARAN</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Nama Anggota Kelompok Source 2:
+							<br>
+							1. AMUDA (Nim : 15220303)
+							<br>
+							2. FIFI ELVIYANI (Nim : 15220304)
+							<br>
+							3. NURAZLINA (Nim : 14221495)
+                            <br><br><br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
